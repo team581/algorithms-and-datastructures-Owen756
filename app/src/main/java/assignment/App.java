e965717e864bdf33eb51e65118df14e4b03aa9cd
Owen <117956892+Owen756@users.noreply.github.com>
2025-06-20 21:26:05 -0700

    fix homework

--- app/src/main/java/assignment/App.java
+++ app/src/main/java/assignment/App.java
@@ -8,36 +8,33 @@ public class App {
       head = head.next;//0(1)
     }
     return head.data; // 0(1)
-  }//0(n)
+  }//0(n)final algorithimic complexity
 
   public static void set(LinkedList list, int index, int value) {
     var head = list.head; // 0(1)
     for(int i = 0; i < index; i++){//0(n)
       head = head.next;//0(1)
     } head.data = value;// 0(1)
-  }//0(n)
+  }//0(n)final algorithimic complexity
 
   public static void remove(LinkedList list, int index) {
     var head = list.head; //0(1)
     for(int i = 0; i < index; i++){//0(n)
       head = head.next;//0(1)
     } head.next = head.next.next;//0(1)
-  }//0(n)
+  }//0(n) final algorithimic complexity
 
   public static LinkedList reverse(LinkedList list) {
-    LinkedList.Node prev = null;//0(1)
-    var head = list.head;//0(1)
-    var next = list.head.next;//0(1)
-
-    while(head != null){//0(n)
-      next = head.next; // O(1)
-      head.next = prev; // O(1)
-      prev = head; // O(1)
-      head = next;//0(1)
+    LinkedList reversedList = new LinkedList();//0(1)
+    LinkedList.Node head = list.head;//0(1)
+    for (int i = 0; i < list.length(); i++){//0(n)
+    reversedList.prepend(head.data);//0(1)
+    head = head.next;//0(1)
     }
-    list.head = prev;//0(1)
-    return list;//0(1)
-  }//0(n)
+    return reversedList;
+    }
+    //0(n)final algorithimic complexity
+  
 
   public static boolean isSortedAscending(LinkedList list) {
     for(int i = 0; i < list.length(); i++){//0(n)
@@ -48,6 +45,6 @@ public class App {
     }
     return true;//0(1)
   }
-//0(n^2)
+//0(n^2)final algorithimic complexity
   private App() {}
 }
